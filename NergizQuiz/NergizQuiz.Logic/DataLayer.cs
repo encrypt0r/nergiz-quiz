fb4f08c2881e81576158a7ecb24a4ea288a7500f
encrypt0r <encryptor@outlook.com>
2016-03-04 20:24:29 +0300

    The logic layer is good enough to test now

---
+++ NergizQuiz/NergizQuiz.Logic/DataLayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace NergizQuiz.Logic
+{
+    public static class DataLayer
+    {
+        #region Members
+        private static Random randomGenerator;
+        private static List<XElement> listOfQuestions;
+        #endregion // Members
+        #region Construction
+        static DataLayer()
+        {
+            randomGenerator = new Random();
+            XElement data = XElement.Load("Data\\Questions.xml");
+            
+            listOfQuestions = data.Elements().ToList();
+        }
+        #endregion // Construction
+        #region Public Functions
+        static public XElement GetNextQuestion()
+        {
+            XElement question;
+            int randomNumber = randomGenerator.Next(0, 3);
+            question = listOfQuestions[randomNumber];
+
+            return question;
+        }
+        #endregion // Public Function
+    }
+}
